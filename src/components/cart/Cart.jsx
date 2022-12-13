de59657c3ba86efb3e38b28dc3e01250faee2ad2
satishkvesteinn <satishkvish12@gmail.com>
2022-12-13 11:16:07 +0530

    version 1.1.2

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,55 +3,58 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import './cart.scss'
 
+const data = [
+    {
+        id: 1,
+        title: 1000,
+        desc: "Total Registered Students"
+    },
+    {
+        id: 2,
+        title: 6,
+        desc: "Request Pending"
+    },
+    {
+        id: 3,
+        title: 500,
+        desc: "CA"
+    },
+    {
+        id: 4,
+        title: 350,
+        desc: "Lawyers"
+    },
+    {
+        id: 5,
+        title: 100,
+        desc: "Doctors"
+    },
+    {
+        id: 6,
+        title: 200,
+        desc: "Students"
+    },
+]
+
 export default function Cart() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid container rowSpacing={3} columnSpacing={{ xs: 2, sm: 3, md: 4 }}>
-                <Grid xs={12} md={6} lg={4}>
-                    <div className="cart d-flex">
-                        <i className="bi bi-person cart-icon"></i>
-                        <div className="cart-content d-flex justify-content-center align-items-start flex-column">
-                            <h5 className='cart-title'>1000+</h5>
-                            <p className='cart-desc'>Total Registered Students</p>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid xs={12} md={6} lg={4}>
-                    <div className="cart d-flex">
-                        <i className="bi bi-person cart-icon"></i>
-                        <div className="cart-content d-flex justify-content-center align-items-start flex-column">
-                            <h5 className='cart-title'>1000+</h5>
-                            <p className='cart-desc'>Total Registered Students</p>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid xs={12} md={6} lg={4}>
-                    <div className="cart d-flex">
-                        <i className="bi bi-person cart-icon"></i>
-                        <div className="cart-content d-flex justify-content-center align-items-start flex-column">
-                            <h5 className='cart-title'>1000+</h5>
-                            <p className='cart-desc'>Total Registered Students</p>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid xs={12} md={6} lg={4}>
-                    <div className="cart d-flex">
-                        <i className="bi bi-person cart-icon"></i>
-                        <div className="cart-content d-flex justify-content-center align-items-start flex-column">
-                            <h5 className='cart-title'>1000+</h5>
-                            <p className='cart-desc'>Total Registered Students</p>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid xs={12} md={6} lg={4}>
-                    <div className="cart d-flex">
-                        <i className="bi bi-person cart-icon"></i>
-                        <div className="cart-content d-flex justify-content-center align-items-start flex-column">
-                            <h5 className='cart-title'>1000+</h5>
-                            <p className='cart-desc'>Total Registered Students</p>
-                        </div>
-                    </div>
-                </Grid>
+                {
+                    data.map(item => {
+                        return (
+                            <Grid xs={12} md={6} lg={4} key={item.id}>
+                                <div className="cart d-flex">
+                                    <i className="bi bi-person cart-icon"></i>
+                                    <div className="cart-content d-flex justify-content-center align-items-start flex-column">
+                                        <h5 className='cart-title'>{item.title}+</h5>
+                                        <p className='cart-desc'>{item.desc}</p>
+                                    </div>
+                                </div>
+                            </Grid>
+                        )
+                    })
+                }
             </Grid>
         </Box>
     );
